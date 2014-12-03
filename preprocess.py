f8f05f9c9bc03b6f84c9a15f06e413c087d306b7
Maxwell Siegelman <Maxwellsiegelman@Maxwells-MacBook-Pro.local>
2014-12-02 19:29:47 -0800

    This is kinda of working, contains bootstrapping. ToDo: add text features, look at the parameters of the model we trained/possibly do feature selection, and see what it looks like when predicting some actual data to make sure things are actually working.:

--- preprocess.py
+++ preprocess.py
@@ -2,8 +2,12 @@ from json import loads
 import pylab as pl
 import numpy as np
 import scipy
+import random
 from sklearn import preprocessing
 from sklearn import linear_model
+from sklearn import svm
+from sklearn.naive_bayes import GaussianNB
+from sklearn.feature_extraction.text import CountVectorizer
 import datetime
 #not sure i did these imports right yet, lets see if i get a chance to test
 
@@ -21,13 +25,23 @@ trainingReviews = []
 testReviewActual = []
 # List of integers
 trainingReviewActual = []
+#Label 1 or zero based on whether the review actually has more than 10 upvotes
+testReviewActualClassification = []
+#Label 1 or zero based on whether the review actually has more than 10 upvotes
+trainingReviewActualClassification = []
+#Buckets for how many upvotes the review has 
+testReviewActualClassification = []
+#Buckets for how many upvotes the review has
+trainingReviewActualClassification = []
+
 # Dict of userid:user object
 users = {}
 # Dict of businessid: business object
 businesses = {}
 
-# Starting with lists because they are what I know, 
-# I figure later we can go through and convert each one into a numpy array
+testReviewsEvenDistribution = []
+testReviewsEvenDistributionActual = []
+testReviewsEvenDistributionActualClassification = []
 
 def parseReviewDate(date):
 	#get the date... a bit messy
@@ -78,6 +92,7 @@ def getUserFeatures(users, userId):
 
 # Right now just gets the features in the review, not the text
 def extractReviewFeatures(review):
+	ngram_vectorizer = CountVectorizer(analyzer='char', )
 	features = []
 	features.append(review['stars'])
 	features.append(len(review['text']))
@@ -86,6 +101,9 @@ def extractReviewFeatures(review):
 	businessFeatures = getBusinessFeatures(businesses, review['business_id'])
 	features.extend(businessFeatures)
 	features.append(parseReviewDate(review['date']))
+	features.append()
+	for i in xrange(len(features)):
+		features[i] = float(features[i])
 	return features
 
 # Get all the users
@@ -99,21 +117,56 @@ with open(businesses_filename, 'r') as businesses_file:
 		business = (loads(line))
 		businesses[business['business_id']] = business
 
-numTrainingExamples = 500
-numTestExamples = 100
+numTrainingExamples = 100000
+numTestExamples = 1000
+cutoff = 20
+numAboveCutoff = 0
+with open(review_filename, 'r') as review_file:
+	for line in review_file:
+  		review = loads(line)
+  		numUpvotes = review['votes']['funny'] + review['votes']['useful'] + review['votes']['cool']
+  		if numUpvotes > cutoff:
+  			numAboveCutoff += 1
+print numAboveCutoff
+
 with open(review_filename, 'r') as review_file:
 	n = 0
   	for line in review_file:
   		review = loads(line)
+  		numUpvotes = 0
   		if n > numTrainingExamples + numTestExamples:
   			break
   		if n > numTrainingExamples:
-  			testReviews.append(extractReviewFeatures(review))
-  			testReviewActual.append(review['votes']['funny'] + review['votes']['useful'] + review['votes']['cool'])
+  			numUpvotes = review['votes']['funny'] + review['votes']['useful'] + review['votes']['cool']
+  			if len(testReviews) < numTestExamples:
+  				testReviews.append(extractReviewFeatures(review))
+  				testReviewActual.append(numUpvotes)
+  				if numUpvotes > cutoff:
+  					testReviewActualClassification.append(1)
+  				else:
+  					testReviewActualClassification.append(0)
+  			if n%2 == 1:
+  				if numUpvotes > cutoff:
+  					testReviewsEvenDistributionActualClassification.append(1)
+  					testReviewsEvenDistribution.append(extractReviewFeatures(review))
+  					testReviewsEvenDistributionActual.append(numUpvotes)
+  					n+=1
+  			else:
+  				if numUpvotes < cutoff:
+  					testReviewsEvenDistributionActualClassification.append(0)
+  					testReviewsEvenDistribution.append(extractReviewFeatures(review))
+  					testReviewsEvenDistributionActual.append(numUpvotes)
+  					n+=1
   		else:
+  			numUpvotes = review['votes']['funny'] + review['votes']['useful'] + review['votes']['cool']
   			trainingReviews.append(extractReviewFeatures(review))
-  			trainingReviewActual.append(review['votes']['funny'] + review['votes']['useful'] + review['votes']['cool'])
-  		n += 1
+  			trainingReviewActual.append(numUpvotes)
+  			if numUpvotes > cutoff:
+  				trainingReviewActualClassification.append(1)
+  			else:
+  				trainingReviewActualClassification.append(0)
+  			n += 1
+
 
 # print 'testReviews:'
 # print testReviews
@@ -124,15 +177,107 @@ with open(review_filename, 'r') as review_file:
 # print 'trainingReviewActual:'
 # print trainingReviewActual 
 
+
 test_design_matrix = np.array(testReviews)
 training_design_matrix = np.array(trainingReviews)
+
+modelScaler = preprocessing.StandardScaler().fit(training_design_matrix)
+test_design_matrix_scaled = modelScaler.transform(test_design_matrix)
+training_design_matrix_scaled = modelScaler.transform(training_design_matrix)
+
+even_test_design_matrix = np.array(testReviewsEvenDistribution)
+even_test_design_matrix_scaled = modelScaler.transform(even_test_design_matrix)
 
 test_outputs = np.array(testReviewActual)
 training_outputs = np.array(trainingReviewActual)
 
-# print test_design_matrix
-# print training_design_matrix
+training_outputs_classification = np.array(trainingReviewActualClassification)
+test_outputs_classification = np.array(testReviewActualClassification)
+even_test_outputs = np.array(testReviewsEvenDistributionActual)
+even_test_outputs_classification = np.array(testReviewsEvenDistributionActualClassification)
 
+#bootstrapping
+numModels = 40
+bootstrapDatasetSize = 1000
+totalTrainScore = 0
+totalTestScore = 0
+evenDistTestScore = 0
+for i in xrange(numModels):
+  	newModelSet = []
+  	newModelSetActual = []
+  	newModelSetActualClassification = []
+  	usedNumbers = {}
+  	j = 0
+  	while j < bootstrapDatasetSize:
+  		rand = random.randint(0, len(trainingReviewActual)-1)
+  		while rand in usedNumbers:
+  			rand = random.randint(0, len(trainingReviewActual)-1)
+  		if j%2 == 1:
+  			if trainingReviewActualClassification[rand] == 1:
+  				newModelSet.append(trainingReviews[rand])
+  				newModelSetActualClassification.append(1)
+  				newModelSetActual.append(trainingReviewActual[rand])
+  				j += 1
+  				usedNumbers[rand] = 1
+  		else:
+  			if trainingReviewActualClassification[rand] == 0:
+  				newModelSet.append(trainingReviews[rand])
+  				newModelSetActualClassification.append(0)
+  				newModelSetActual.append(trainingReviewActual[rand])
+  				j += 1
+  				usedNumbers[rand] = 1
+  	scaler = preprocessing.StandardScaler().fit(np.array(newModelSet))
+	trainMatrix = scaler.transform(np.array(newModelSet))
+	trainOutput = np.array(newModelSetActual)
+	trainOutputClassification = np.array(newModelSetActualClassification)
+	temporaryTestset = scaler.transform(test_design_matrix)
+	temporaryEvenTestset = scaler.transform(even_test_design_matrix)
+	clf = linear_model.LinearRegression()
+	totalTrainScore += clf.fit(trainMatrix, trainOutputClassification).score(trainMatrix, trainOutputClassification)
+	totalTestScore += clf.fit(trainMatrix, trainOutputClassification).score(temporaryTestset, test_outputs_classification)
+	evenDistTestScore += clf.fit(trainMatrix, trainOutputClassification).score(temporaryEvenTestset, even_test_outputs_classification)
 
+print "Average training score is " + str(totalTrainScore/numModels)
+print "Average test score is " + str(totalTestScore/numModels)
+print "Average test score on even distribution is " + str(evenDistTestScore/numModels)
+
+print "Linear Regression training score"
 clf = linear_model.LinearRegression()
-clf.fit(training_design_matrix, training_outputs)
+print clf.fit(training_design_matrix_scaled, training_outputs).score(training_design_matrix_scaled, training_outputs)
+
+print "\nLinear Regression test score"
+print clf.fit(training_design_matrix_scaled, training_outputs).score(test_design_matrix_scaled, test_outputs)
+print "\nLinear Regression score on evenly distributed test"
+print clf.fit(training_design_matrix_scaled,training_outputs).score(even_test_design_matrix_scaled, even_test_outputs)
+
+print "\nLogistic Regression training score"
+logisticRegression = linear_model.LogisticRegression()
+print logisticRegression.fit(training_design_matrix_scaled, training_outputs_classification).score(training_design_matrix_scaled, training_outputs_classification)
+
+print "\nLogistic regression test score"
+print logisticRegression.fit(training_design_matrix_scaled, training_outputs_classification).score(test_design_matrix_scaled, test_outputs_classification)
+print "\nLogistic regression score on evenly distributed test"
+print logisticRegression.fit(training_design_matrix_scaled,training_outputs_classification).score(even_test_design_matrix_scaled, even_test_outputs_classification)
+
+print "\nNaive Bayes training score"
+naiveBayes = GaussianNB()
+print naiveBayes.fit(training_design_matrix_scaled, training_outputs_classification).score(training_design_matrix_scaled, training_outputs_classification)
+print "\nNB score on evenly distributed test"
+print naiveBayes.fit(training_design_matrix_scaled,training_outputs_classification).score(even_test_design_matrix_scaled, even_test_outputs_classification)
+
+print "\nNaive Bayes test score"
+print naiveBayes.fit(training_design_matrix_scaled, training_outputs_classification).score(test_design_matrix_scaled, test_outputs_classification)
+
+print "\nSVM (SVC) training scorez"
+supportVecMachine = svm.SVC(tol=1e-2, max_iter=3000)
+#print supportVecMachine.fit(training_design_matrix_scaled,training_outputs_classification).score(training_design_matrix_scaled, training_outputs_classification)
+
+print "\nSVM (SVC) test score"
+#print supportVecMachine.fit(training_design_matrix_scaled,training_outputs_classification).score(test_design_matrix_scaled, test_outputs_classification)
+print "\nSVM score on evenly distributed test"
+#print supportVecMachine.fit(training_design_matrix_scaled,training_outputs_classification).score(even_test_design_matrix_scaled, even_test_outputs_classification)
+#
+#TODO:
+#new scoring method
+#get the text features
+#
